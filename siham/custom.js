1e9476c8bcca10b282cf4be9deb446c626a12cca
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2018-07-10 11:03:06 +0200

    dossier-agent: hide phone numbers (values not updated, LDAP values are better) (GLPI UP1#72117)

--- siham/custom.js
+++ siham/custom.js
@@ -8,8 +8,11 @@ function searchLeaf(selector, innerText) {
     });
 }
 
-function searchLabel(selector, innerText) {
+function searchLeaf_then_table_tr(selector, innerText) {
     return searchLeaf(selector, innerText).closest("table").closest("tr");
+}
+function searchLeaf_then_fieldset(selector, innerText) {
+    return searchLeaf(selector, innerText).closest("fieldset");
 }
 function searchSection(innerText) {
     return searchLeaf(".FORMSECTION_TITLE1 td", innerText).closest("h3").closest("table").closest("tr");
@@ -21,6 +24,12 @@ searchSection("Affectation").addClass("block-Affectation");
 searchSection("Avancement d'échelon").addClass("block-Avancement-echelon");
 searchSection("Ancienneté").addClass("hideIt");
 
-searchLabel("label", 'Indicateur célibataire géographique').addClass('hideIt hideNextRow');
-searchLabel(".block-Affectation label", 'Poste').addClass('hideIt hideNextRow');
-searchLabel(".block-Avancement-echelon label", 'Spécialité').addClass('hideIt hideNextRow');
+searchLeaf_then_table_tr("label", 'Indicateur célibataire géographique').addClass('hideIt hideNextRow');
+searchLeaf_then_table_tr(".block-Affectation label", 'Poste').addClass('hideIt hideNextRow');
+searchLeaf_then_table_tr(".block-Avancement-echelon label", 'Spécialité').addClass('hideIt hideNextRow');
+
+searchLeaf_then_fieldset("label", 'Téléphone portable perso').addClass('hideIt hideNextRow');
+searchLeaf_then_fieldset("label", 'Téléphone fixe perso').addClass('hideIt hideNextRow');
+searchLeaf_then_fieldset("label", 'Téléphone fixe pro principal').addClass('hideIt hideNextRow');
+searchLeaf_then_fieldset("label", 'Courrier électronique perso').addClass('hideIt hideNextRow');
+searchLeaf_then_fieldset("label", 'Courrier électronique pro').addClass('hideIt hideNextRow');
